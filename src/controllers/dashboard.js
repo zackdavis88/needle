c6c9987f325017a767bb2b290f1e5f40bcb0ca94
Zack Davis <zack.davis88@hotmail.com>
2020-08-09 11:06:46 -0700

    removing old dashboard endpoint

--- src/controllers/dashboard.js
+++ src/controllers/dashboard.js
@@ -2,65 +2,6 @@ import Membership from "../models/membership";
 import Story from "../models/story";
 import Project from "../models/project";
 
-const get = (req, res) => {
-  const {user} = req;
-  Membership
-    .find({user: user._id})
-    .sort({createdOn: "asc"})
-    .populate("project")
-    .exec((err, memberships) => {
-      if(err)
-        return res.fatalError(err);
-
-      Story
-        .find()
-        .or([
-          {owner: user._id},
-          {creator: user._id}
-        ])
-        .sort({createdOn: "asc"})
-        .populate("project", "-description")
-        .populate("creator", "-_id username displayName")
-        .populate("owner", "-_id username displayName")
-        .exec((err, stories) => {
-          if(err)
-            return res.fatalError(err);
-
-          const result = {
-            projects: memberships.reduce((prev, {project, roles}) => {
-              if(!project.isActive)
-                return prev;
-              
-              return prev.concat({
-                id: project._id,
-                name: project.name,
-                description: project.description,
-                isPrivate: project.isPrivate,
-                isActive: project.isActive,
-                createdOn: project.createdOn,
-                updatedOn: project.updatedOn,
-                roles
-              });
-            }, []),
-            stories: stories.map(story => ({
-              id: story._id,
-              name: story.name,
-              creator: story.creator,
-              owner: story.owner,
-              project: {
-                id: story.project._id,
-                name: story.project.name
-              },
-              createdOn: story.createdOn,
-              updatedOn: story.updatedOn
-            }))
-          };
-
-          return res.success("user dashboard has been successfully retrieved", result);
-        });
-    });
-};
-
 const getProjects = (req, res) => {
   const {user, memberProjects} = req;
   const {
@@ -154,7 +95,6 @@ const getStories = (req, res) => {
 };
 
 export default {
-  get,
   getProjects,
   getStories
 };
--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -4,10 +4,6 @@ import authController from "../controllers/auth";
 import authValidator from "../validators/auth";
 
 const dashboardRoutes = (router) => {
-  router.route("/dashboard")
-    .all(authValidator.jwtHeader, authController.authenticateToken)
-    .get(dashboardController.get);
-
   router.route("/dashboard/projects")
     .all(authValidator.jwtHeader, authController.authenticateToken)
     .get(
