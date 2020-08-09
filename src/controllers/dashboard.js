9adbfcfc980b73055fef329352fc44f9a9e2a764
Zack Davis <zack.davis88@hotmail.com>
2020-08-09 10:05:43 -0700

    added paginated and split dashboard routes

--- src/controllers/dashboard.js
+++ src/controllers/dashboard.js
@@ -1,5 +1,6 @@
 import Membership from "../models/membership";
 import Story from "../models/story";
+import Project from "../models/project";
 
 const get = (req, res) => {
   const {user} = req;
@@ -60,6 +61,100 @@ const get = (req, res) => {
     });
 };
 
+const getProjects = (req, res) => {
+  const {user, memberProjects} = req;
+  const {
+    page,
+    totalPages,
+    itemsPerPage
+  } = req.paginationData;
+  const pageOffset = (page - 1) * itemsPerPage;
+  Project
+    .find({_id: {$in: memberProjects}, isActive: true})
+    .sort({createdOn: "desc"})
+    .select("-description -isActive")
+    .skip(pageOffset)
+    .limit(itemsPerPage)
+    .exec((err, projects) => {
+      if(err)
+        return res.fatalError(err);
+      
+      const projectIds = projects.map(project => project._id);
+      Membership.find({user: user._id, project: {$in: projectIds}}, (err, memberships) => {
+        if(err)
+          return res.fatalError(err);
+        
+        const result = {
+          page,
+          totalPages,
+          itemsPerPage,
+          projects: projects.map(project => ({
+            id: project._id,
+            name: project.name,
+            isPrivate: project.isPrivate,
+            createdOn: project.createdOn,
+            updatedOn: project.updatedOn,
+            roles: memberships.find(membership => membership.project.toString() === project._id.toString()).roles
+          }))
+        };
+
+        res.success("dashboard projects successfully retrieved", result);
+      });
+    });
+};
+
+const getStories = (req, res) => {
+  const {user, memberProjects} = req;
+  const {
+    page,
+    totalPages,
+    itemsPerPage
+  } = req.paginationData;
+  const pageOffset = (page - 1) * itemsPerPage;
+  Story
+    .find({
+      $and: [
+        {$or: [
+          {owner: user._id},
+          {creator: user._id}
+        ]},
+        {project: {$in: memberProjects}}
+      ]
+    })
+    .populate("creator", "-_id username displayName")
+    .populate("owner", "-_id username displayName")
+    .populate("project", "-description")
+    .sort({createdOn: "desc"})
+    .skip(pageOffset)
+    .limit(itemsPerPage)
+    .exec((err, stories) => {
+      if(err)
+        return res.fatalError(err);
+      
+      const result = {
+        page,
+        itemsPerPage,
+        totalPages,
+        stories: stories.map(story => ({
+          id: story._id,
+          name: story.name,
+          creator: story.creator,
+          owner: story.owner,
+          project: {
+            id: story.project._id,
+            name: story.project.name
+          },
+          createdOn: story.createdOn,
+          updatedOn: story.updatedOn
+        }))
+      };
+
+      res.success("dashboard stories successfully retrieved", result);
+    });
+};
+
 export default {
-  get
+  get,
+  getProjects,
+  getStories
 };
--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -1,4 +1,5 @@
 import dashboardController from "../controllers/dashboard";
+import dashboardValidator from "../validators/dashboard";
 import authController from "../controllers/auth";
 import authValidator from "../validators/auth";
 
@@ -6,6 +7,20 @@ const dashboardRoutes = (router) => {
   router.route("/dashboard")
     .all(authValidator.jwtHeader, authController.authenticateToken)
     .get(dashboardController.get);
+
+  router.route("/dashboard/projects")
+    .all(authValidator.jwtHeader, authController.authenticateToken)
+    .get(
+      dashboardValidator.getProjects,
+      dashboardController.getProjects
+    );
+
+  router.route("/dashboard/stories")
+    .all(authValidator.jwtHeader, authController.authenticateToken)
+    .get(
+      dashboardValidator.getStories,
+      dashboardController.getStories
+    );    
 };
 
 export default dashboardRoutes;
